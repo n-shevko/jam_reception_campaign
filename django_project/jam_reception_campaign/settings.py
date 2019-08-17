c8784d54315c557a766bbd1522ec41d5367b0e78
nikos <nikdranik2@gmail.com>
2019-08-17 13:20:02 +0300

    db models

--- django_project/jam_reception_campaign/settings.py
+++ django_project/jam_reception_campaign/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'jam_reception_campaign.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'data',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
