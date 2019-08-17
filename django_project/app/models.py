b1e7a22b8e38788a0ed3e02c060d182b599b0015
nikos <nikdranik2@gmail.com>
2019-08-17 14:10:28 +0300

    Colleges model

--- django_project/app/models.py
+++ django_project/app/models.py
@@ -15,11 +15,22 @@ class Applicants(models.Model):
     last_update_date = models.DateField()
 
 
+class Colleges(models.Model):
+    college_name = models.CharField(max_length=50)
+    college_desc = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    disable_flag = models.CharField(max_length=1)
+    creation_date = models.DateField(auto_now=True)
+    last_update_date = models.DateField()
+
+
 class Specialty(models.Model):
     spec_id = models.IntegerField()
     spec_name = models.CharField(max_length=50)
     spec_desc = models.CharField(max_length=250)
-    colledge_id = models.IntegerField()
+    colledge_id = models.ForeignKey(Colleges, on_delete=models.SET_NULL, null=True)
     disable_flag = models.CharField(max_length=1)
     creation_date = models.DateField(auto_now=True)
     last_update_date = models.DateField()
