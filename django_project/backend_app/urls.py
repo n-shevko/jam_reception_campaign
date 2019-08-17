930c6815ff04018f89e27affb07b6807f10ec1e5
hartred <artiomse@gmail.com>
2019-08-17 16:08:22 +0300

    fixes to resolve urls and templates

--- django_project/backend_app/urls.py
+++ django_project/backend_app/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
+
+app_name = 'backend_app'
 
 urlpatterns = [
     path('', views.BackendIndex.as_view(), name='index'),
--- django_project/backend_app/views.py
+++ django_project/backend_app/views.py
@@ -3,4 +3,4 @@ from django.views.generic import TemplateView
 
 
 class BackendIndex(TemplateView):
-    template_name = '../templates/backend_app/index.html'
+    template_name = 'backend_app/index.html'
