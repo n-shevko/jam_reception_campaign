9a4ca03b003959d99af11214b60938c4c192d042
hartred <artiomse@gmail.com>
2019-08-17 16:02:07 +0300

    changed project layout

--- django_project/backend_app/views.py
+++ django_project/backend_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class BackendIndex(TemplateView):
+    template_name = '../templates/backend_app/index.html'
--- django_project/jam_reception_campaign/settings.py
+++ django_project/jam_reception_campaign/settings.py
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'jam_reception_campaign.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +66,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
 ]
 
 WSGI_APPLICATION = 'jam_reception_campaign.wsgi.application'
