c744b9d7a130baa939541bfbc7dd138970639a2c
nikos <nikdranik2@gmail.com>
2019-08-17 18:44:34 +0300

    Applications model

--- django_project/app/views.py
+++ django_project/app/views.py
@@ -7,24 +7,6 @@ def index(request):
     })
 
 
-
-    # first_name = models.CharField(max_length=50)
-    # middle_name = models.CharField(max_length=50)
-    # last_name = models.CharField(max_length=50)
-    # date_of_birthday = models.DateField()
-    # citizenship = models.CharField(max_length=50)
-    # passport_num = models.CharField(max_length=50)
-    # personal_id = models.CharField(max_length=50)
-    # passport_exp_date = models.DateField()
-    # photo_link = models.ImageField()
-    # creation_date = models.DateField(auto_now=True)
-    # last_update_date = models.DateField()
-    # sex = models.CharField(
-    #     max_length=1,
-    #     choices=SEX,
-    #     default=FEMALE,
-    # )
-
 def new_application(request):
     education_options = {
         "Minsk": {
@@ -47,7 +29,12 @@ def new_application(request):
         ],
         "subjects": [['Математика', 'Русский язык'], ['Английский язык', 'Изо']],
         "tests": ["Математика", "Русский"],
-        "education_options": education_options
+        "education_options": education_options,
+        "cheaters": [
+            "Победитель олимпиады",
+            "Сирота",
+            "Спортсмены"
+        ]
     })
 
 
