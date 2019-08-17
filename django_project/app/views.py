23a373c7aa5a87b9252c99896a0ff5a6ce84fcef
nikos <nikdranik2@gmail.com>
2019-08-17 14:56:02 +0300

    Applications model

--- django_project/app/views.py
+++ django_project/app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+
 
 def index(request):
     return render_to_response('index.html', {})
 
 
 def new_application(request):
-    d = 34
     return render_to_response('application_form.html', {})
+
+
+def campagin_status(request):
+    return render_to_response('campagin_status.html', {})
