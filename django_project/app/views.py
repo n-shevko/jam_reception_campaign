fb62ef4bb73046cd659bea63fed37a3f6d67c148
nikos <nikdranik2@gmail.com>
2019-08-17 14:46:52 +0300

    Applications model

--- django_project/app/views.py
+++ django_project/app/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 
 def index(request):
     return render_to_response('index.html', {})
+
+
+def new_application(request):
+    d = 34
+    return render_to_response('application_form.html', {})
