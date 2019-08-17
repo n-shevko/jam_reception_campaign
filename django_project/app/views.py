8dcf20ee913a349af83dab4cdaac92a4f677c697
nikos <nikdranik2@gmail.com>
2019-08-17 18:23:13 +0300

    Applications model

--- django_project/app/views.py
+++ django_project/app/views.py
@@ -7,18 +7,47 @@ def index(request):
     })
 
 
+
+    # first_name = models.CharField(max_length=50)
+    # middle_name = models.CharField(max_length=50)
+    # last_name = models.CharField(max_length=50)
+    # date_of_birthday = models.DateField()
+    # citizenship = models.CharField(max_length=50)
+    # passport_num = models.CharField(max_length=50)
+    # personal_id = models.CharField(max_length=50)
+    # passport_exp_date = models.DateField()
+    # photo_link = models.ImageField()
+    # creation_date = models.DateField(auto_now=True)
+    # last_update_date = models.DateField()
+    # sex = models.CharField(
+    #     max_length=1,
+    #     choices=SEX,
+    #     default=FEMALE,
+    # )
+
 def new_application(request):
+    education_options = {
+        "Minsk": {
+            "BGU": ["Povar", "Kuharka"],
+            "BGUIR": ["Koder"]
+        },
+        "Gomel": {
+            "BELGUT": ['Sviaz'],
+            "GTEK": ["Proger"]
+        }
+    }
     return render_to_response('app/application_form.html', {
         "title": "Подача заявления",
         "fields": [
-            'Гражданство',
-            'Номер паспорта',
-            'Личный номер',
-            'Дата выдачи',
-            'Фотография'
+            ['Гражданство', 'citizenship'],
+            ['Номер паспорта', 'passport_num'],
+            ['Личный номер', 'personal_id'],
+            ['Дата выдачи', 'passport_exp_date'],
+            ['Фотография', 'photo']
         ],
         "subjects": [['Математика', 'Русский язык'], ['Английский язык', 'Изо']],
-        "tests": ["Математика", "Русский"]
+        "tests": ["Математика", "Русский"],
+        "education_options": education_options
     })
 
 
